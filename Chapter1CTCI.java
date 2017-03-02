9612fcbbdcf58f5ce665c11f70ae15cf9ebc060b
charlesma4 <charlesma4@gmail.com>
2017-03-02 16:41:11 -0500

    Added  problem 2

--- Chapter1CTCI.java
+++ Chapter1CTCI.java
@@ -14,6 +14,12 @@ public class Chapter1CTCI {
     System.out.println("BbAaCc: "+isUniqueChars2("BbAaCc"));
     System.out.println("Februay: "+isUniqueChars2("Februay"));
 
+    System.out.println("Problem 2:");
+    System.out.println(isPermutation("qwertyuiop","poiuytrewq"));
+    System.out.println(isPermutation("qwertyuiop","poiutyrewq"));
+    System.out.println(isPermutation("",""));
+    System.out.println(isPermutation("q","p"));
+    System.out.println(isPermutation("q","q"));
   }
 
   //returns true if string's chars are all unique, false if not - allowed to use data structures
@@ -46,6 +52,46 @@ public class Chapter1CTCI {
     }
     return true;
   }
+
+  //given two strings, write a method to decide if one is a permutation of the other
+  public static boolean isPermutation(String s, String t) {
+
+    Hashtable<Character, Integer> chars = new Hashtable<Character, Integer>();
+    Hashtable<Character, Integer> chars2 = new Hashtable<Character, Integer>();
+
+    for(char x: s.toCharArray()) {
+      if(chars.contains(x)) {
+        chars.put(x,chars.get(x)+1);
+      }
+      else {
+        chars.put(x,1);
+      }
+    }
+
+    for(char y: t.toCharArray()) {
+      if(chars2.contains(y)) {
+        chars2.put(y,chars2.get(y)+1);
+      }
+      else {
+        chars2.put(y,1);
+      }
+    }
+
+    Set<Character> vals = chars.keySet();
+
+    for(char x: vals) {
+      if(chars2.containsKey(x)&&chars2.get(x)==chars.get(x)) {
+        continue;
+      }
+      else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //replaces all spaces in a string with '%20' - assumed that string has sufficient
+  //space at end to hold additional characters, and you are given true length of string
 
 
 
